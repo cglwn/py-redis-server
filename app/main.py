a94d7c0b28a7c636adecc2a9cf0f4dcd1ac32095
James Cagalawan <james.cagalawan@gmail.com>
2025-07-14 02:31:18 -0700

    Respond with PONG every request

--- app/main.py
+++ app/main.py
@@ -5,7 +5,8 @@ def main():
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
     connection, _ = server_socket.accept()  # wait for client
 
-    connection.sendall(b"+PONG\r\n")
+    while connection.recv(1024):
+        connection.sendall(b"+PONG\r\n")
 
 
 if __name__ == "__main__":
