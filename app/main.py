f6e25c26f31aed17259142e512520085a5004f94
James Cagalawan <james.cagalawan@gmail.com>
2025-07-14 02:54:46 -0700

    Handle concurrency with asyncio

--- app/main.py
+++ app/main.py
@@ -1,12 +1,24 @@
-import socket  # noqa: F401
+import asyncio
+
+
+async def handle_ping(
+    reader: asyncio.StreamReader, writer: asyncio.StreamWriter
+) -> None:
+    while True:
+        _ = await reader.read(1024)
+        writer.write(b"+PONG\r\n")
+        await writer.drain()
+
+
+async def run_server() -> None:
+    server = await asyncio.start_server(handle_ping, "localhost", 6379)
+    async with server:
+        await server.serve_forever()
 
 
 def main():
-    server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    connection, _ = server_socket.accept()  # wait for client
-
-    while connection.recv(1024):
-        connection.sendall(b"+PONG\r\n")
+    loop = asyncio.new_event_loop()
+    loop.run_until_complete(run_server())
 
 
 if __name__ == "__main__":
